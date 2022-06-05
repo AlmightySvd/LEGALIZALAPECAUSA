1a24b0d80670da8671338922d3ceadadfa6e9aa8
NADA <87243706+Qpaltacsme@users.noreply.github.com>
2022-06-05 01:26:21 -0500

    Update gp-dylux.js

--- plugins/gp-dylux.js
+++ plugins/gp-dylux.js
@@ -1,15 +1,15 @@
 let handler = async m => m.reply(`
 
-≡  *Legalizapecausa  ┃ ᴮᴼᵀ*   GRUPO
+≡  *dilux  ┃ ᴮᴼᵀ*   GRUPO
 
 ▢ Grupo WhatsApp 
- https://chat.whatsapp.com/Ed7CLWZDsGX9G9UlHs73Co
+ 
 
 ▢ *Grupo Facebook*
- https://www.facebook.com/SvdRuthless/
+ 
 
 ▢ *Grupo de Facebook*
-• https://www.facebook.com/SvdRuthless/
+• 
 
 
 `.trim())
